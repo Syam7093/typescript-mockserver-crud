5d8573fcc6f5eea9b931ebf731098da073bf1e87
syamsaiyek <msyamsundarsai@gmail.com>
2024-08-16 10:59:01 +0530

    some

--- src/components/UserData.tsx
+++ src/components/UserData.tsx
@@ -5,10 +5,47 @@ const UserData = () => {
   const [main, setmain] = useState('PRAVEEN---');
   const { one, setone } = useContext(usesContext);
 
+  const [show,setshow]=useState<any>([])
+  const handleDelte=(m:string)=>{
+
+    let data=show.filter((e:any)=>{
+      return e.id!==m
+    })
+    setshow(data)
+
+  }
+
   return (
     <div>
       <div style={{ backgroundColor: 'green', overflow: 'hidden' }}>
         <h1 style={styles.movingText}>syam</h1>
+      </div>
+      <button onClick={()=>{
+        setshow([...show,{id:Date.now()}])
+      }}>create</button>
+      <button onClick={()=>{
+        setshow([])
+      }}>clear</button>
+
+      <div>
+        {
+          show.map((e:any)=>{
+            return(
+              <div>
+                <select key={e.id}>
+                  <option>name</option>
+                  <option>age</option>
+                  <option>gender</option>
+                  
+
+                </select>
+                <button onClick={()=>{
+                  handleDelte(e.id)
+                }}>X</button>
+              </div>
+            )
+          })
+        }
       </div>
     </div>
   );
